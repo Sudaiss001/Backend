f55988a2200e5c647dd7de9d63fe3871bc991e17
Sudaiss001 <mustaphaabdulrahman811@gmail.com>
2026-06-17 17:37:26 +0100

    Added files

---
+++ arrays.php
@@ -0,0 +1,22 @@
+<?php
+    $fruits = ['banana', 'apple', 'orange'];
+    //echo $fruits[0];
+    // echo '<pre>';
+    // var_dump($fruits);
+    // echo '<pre>';
+
+    // Mixed array
+    $mixed_arr = [ 12, "banana", true];
+    // echo '<pre>';
+    //var_dump($mixed_arr);
+    // echo '<pre>';
+
+    // associative array
+    $users_arr = [
+        'name'=>'Abdul',
+        'age'=> 20,
+        'hobbies' => ['coding', 'tennis']
+    ];
+     echo '<pre>';
+     var_dump($users_arr);
+     echo '<pre>';
--- conditionals.php
+++ conditionals.php
@@ -5,6 +5,7 @@
     // || OR
     // ! NOT
 
+    //Comparism
     // ==
     // ===
     // !=
@@ -14,5 +15,21 @@
     // <= 
     // --
     // ++
+    /*
+    $age = 10;
+    if ( $age <= 10) {
+        echo "Too young to Harry Potter.";
+    } else if ( $age <= 15 ) {
+        echo "You may watch Harry Potter.";
+    } else {
+     echo "Should watch Harry Potter.";
+    }
+    */
+    $age = 15;
+    $watched = false;
     
-
+    if ( $age >= 12 && !$watched ) {
+        echo "You must watch Harry Potter.";
+    } elseif ( $age >= 15 || !$watched) {
+        echo "You may watch Harry Potter.";
+    }
