aefdc0ca6e533abee7471862a4750cea72802427
Mustapha Abdulrahman <mustaphaabdulrahman811@gmail.com>
2026-08-19 12:54:04 +0100

    Add StateLga class and update result.php to test its methods

---
+++ StateLga.php
@@ -0,0 +1,37 @@
+<?php
+    include_once("App.php");
+
+    class StateLga {
+        use App;
+
+        // Fetch all states
+        public function getAllStates() {
+            $sql = "SELECT * FROM states";
+            return $this->fetchMultiple($sql);
+        }
+
+        // Fetch all LGAs
+        public function getAllLGAs() {
+            $sql = "SELECT * FROM lgas";
+            return $this->fetchMultiple($sql);
+        }
+
+        // Fetch single state by ID
+        public function getStateById($id) {
+            $sql = "SELECT * FROM states WHERE id = ?";
+            return $this->fetchSingle($sql, [$id]);
+        }
+
+        // Fetch single LGA by ID
+        public function getLGAById($id) {
+            $sql = "SELECT * FROM lgas WHERE id = ?";
+            return $this->fetchSingle($sql, [$id]);
+        }
+
+        // Fetch all LGAs belonging to a specific state ID
+        public function getLGAsByStateId($stateId) {
+            $sql = "SELECT * FROM lgas WHERE state_id = ?";
+            return $this->fetchMultiple($sql, [$stateId]);
+        }
+    }
+?>
--- result.php
+++ result.php
@@ -1,10 +1,27 @@
 <?php
     include_once("User.php");
+    include_once("StateLga.php");
 
     $user = new User();
-    $res = $user->allUser();
+    $stateLga = new StateLga();
 
+    // --- Testing User.php ---
+    $res = $user->allUser();
+    echo "<h3>All Users</h3>";
     echo "<pre>";
     var_dump($res);
+    echo "</pre>";
+
+    // --- Testing StateLga.php ---
+    $states = $stateLga->getAllStates();
+    echo "<h3>All States</h3>";
+    echo "<pre>";
+    var_dump($states);
+    echo "</pre>";
+
+    $lgas = $stateLga->getLGAsByStateId(1);
+    echo "<h3>LGAs for State ID 1</h3>";
+    echo "<pre>";
+    var_dump($lgas);
     echo "</pre>";
 ?>
