38efcb494e7ba35495c897c242b81ac5da0fefc9
Ayubs01 <adamujob71@gmail.com>
2026-08-31 13:33:31 +0100

    Added Files.

--- config.php
+++ config.php
@@ -1,8 +1,37 @@
-<?php 
-	
+<?php
+
 	const ENVIRONMENT = 'Test';//Test Live
 
-	$website_title = '';
-	$website_email = '';
+	//DB Config
+	const HOST = 'localhost';
+	const USER = 'root';
+	const PWORD = '';
+	const DB = '';
 
+	const DB_TABLE_USER = 'users';
+
+
+	const WEBSITE_TITLE = '';
+
+
+   const MAIL_SERVER_EMAIL = '';
+   const MAIL_SERVER_PWORD = '';
+	
+
+	$req_method = $_SERVER['REQUEST_METHOD'];
+
+
+	//url
+   $server_name = ENVIRONMENT == 'Test' ? 'http://' : 'https://';
+   $server_name .= $_SERVER['SERVER_NAME'];
+   $uri = $_SERVER['REQUEST_URI'];
+   $app_url = ( strlen( $uri ) > 1 ) ? "$server_name$uri" : "$server_name";
+   
+   //directory
+   $root_dir = dirname( __DIR__ );
+   $cur_dir = dirname( __FILE__ );
+
+   $website_url = '';
+
+ 
 ?>
